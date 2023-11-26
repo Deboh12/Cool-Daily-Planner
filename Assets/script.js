97233c6e93ccf4af8a8a6b0a80a631ede6e67407
Deboh12 <deborah.alexander12@yahoo.com>
2023-11-26 08:05:35 -0600

    set up all hours in html, added event listener for click events on teh save button, added code to apply the past, present, or future class to each time, added code to display current date in the header

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +20,57 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+
+
+$(function () {
+
+  $("#currentDay").text(dayjs().format("dddd, MMMM D"));
+
+  // Add a listener for click events on the save button
+  $(".saveBtn").on("click", function () {
+    var timeBlockId = $(this).parent().attr("id");
+    var eventText = $(this).siblings(".description").val();
+
+    localStorage.setItem(timeBlockId, eventText);
+  });
+
+  function updateHourlyBlocks() {
+    // order of hours
+    var hoursOrder = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+  
+    // Get current hour using Day.js
+    var currentHour = dayjs().hour();
+  
+    // Iterate through each time block
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+      var blockIndex = hoursOrder.indexOf(blockHour);
+      var currentIndex = hoursOrder.indexOf(currentHour);
+
+      $(this).removeClass("past present future");
+  
+      if (blockIndex < currentIndex) {
+        $(this).addClass("past");
+      } else if (blockIndex === currentIndex) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  // Get any user input that was saved in localStorage and set the textarea values
+  function loadSavedEvents() {
+    $(".time-block").each(function () {
+      var savedEvent = localStorage.getItem($(this).attr("id"));
+      $(this).find(".description").val(savedEvent);
+    });
+  }
+  
+  updateHourlyBlocks();
+  loadSavedEvents();
+
+  // Update the hourly blocks every minute to reflect the current time
+  setInterval(updateHourlyBlocks, 60000);
 });
