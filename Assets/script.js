9489cddcd932d987f66ad56df48686cd1076aa61
Deboh12 <deborah.alexander12@yahoo.com>
2023-11-26 08:54:23 -0600

    added meaningful/descriptive comments

--- Assets/script.js
+++ Assets/script.js
@@ -1,30 +1,6 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
-
-
   $(function () {
 
+      // Function to update the displayed date in the header
     function updateDate() {
       $("#currentDay").text(dayjs().format("dddd, MMMM D"));
     }
@@ -34,19 +10,24 @@
       $("#currentDay").after("<p class='text-success'>" + message + "</p>");
     }
   
+      // Initial update of the date in the header
     updateDate();
   
     // Add a listener for click events on the save button
     $(".saveBtn").on("click", function () {
+          // Get the ID of the parent time block
       var timeBlockId = $(this).parent().attr("id");
+      // Get the text from the textarea sibling of the clicked button
       var eventText = $(this).siblings(".description").val();
   
+      // Save the event text to localStorage using the time block ID as the key
       localStorage.setItem(timeBlockId, eventText);
   
       // Display message in the header
       displayMessage("Appointment added to local storage");
     });
   
+    // Function to update the styling of hourly time blocks based on the current time
     function updateHourlyBlocks() {
       // order of hours
       var hoursOrder = [9, 10, 11, 12, 1, 2, 3, 4, 5];
@@ -56,12 +37,17 @@
     
       // Iterate through each time block
       $(".time-block").each(function () {
+        // Get the hour from the time block ID
         var blockHour = parseInt($(this).attr("id").split("-")[1]);
+        // Get the index of the block hour in the ordered array
         var blockIndex = hoursOrder.indexOf(blockHour);
+        // Get the index of the current hour in the ordered array
         var currentIndex = hoursOrder.indexOf(currentHour);
   
+        // Remove existing styling classes
         $(this).removeClass("past present future");
     
+        // Add appropriate styling class based on the relationship between block and current hours
         if (blockIndex < currentIndex) {
           $(this).addClass("past");
         } else if (blockIndex === currentIndex) {
@@ -72,14 +58,17 @@
       });
     }
   
-    // Get any user input that was saved in localStorage and set the textarea values
+    // Function to load saved events from localStorage and set textarea values
     function loadSavedEvents() {
       $(".time-block").each(function () {
+        // Get the saved event from localStorage based on the time block ID
         var savedEvent = localStorage.getItem($(this).attr("id"));
+        // Set the textarea value with the saved event text
         $(this).find(".description").val(savedEvent);
       });
     }
     
+    // Initial update of hourly blocks and loading of saved events
     updateHourlyBlocks();
     loadSavedEvents();
   
