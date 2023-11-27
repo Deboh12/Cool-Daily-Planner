0a80f0f9b0e86d9347269d0398f24e60e5adf46a
Deboh12 <deborah.alexander12@yahoo.com>
2023-11-26 18:08:06 -0600

    redid HTML and JavaScript because scheduler was not working as expected

--- Assets/script.js
+++ Assets/script.js
@@ -1,80 +1,78 @@
-  $(function () {
+$(function () {
+  // Function to update the displayed date in the header
+  function updateDate() {
+    $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
+  }
 
-      // Function to update the displayed date in the header
-    function updateDate() {
-      $("#currentDay").text(dayjs().format("dddd, MMMM D"));
+  // Function to display a message in the header
+  function displayMessage(message, isSuccess) {
+    var messageClass = isSuccess ? 'text-success' : 'text-danger';
+    var messageElement = $("<p>").addClass(messageClass).text(message);
+    $("#currentDay").after(messageElement);
+  }
+
+  // Initial update of the date in the header
+  updateDate();
+
+  // Function to generate time blocks
+  function generateTimeBlocks() {
+    var currentHour = dayjs().hour();
+
+    for (var hour = 9; hour <= 17; hour++) {
+      var timeBlock = $('<div>').attr('id', 'hour-' + hour).addClass('row time-block');
+      var displayHour = (hour > 12) ? hour - 12 + "PM" : hour + "AM";
+
+      if (hour < currentHour) {
+        timeBlock.addClass("past");
+      } else if (hour === currentHour) {
+        timeBlock.addClass("present");
+      } else {
+        timeBlock.addClass('future');
+      }
+
+      var hourDiv = $('<div>').addClass('col-2 col-md-1 hour text-center py-3').text(displayHour);
+      var textarea = $('<textarea>').addClass("col-8 col-md-10 description").attr('rows', 3);
+      var saveButton = $('<button>').addClass('btn saveBtn col-2 col-md-1').attr('aria-label', 'save').html('<i class="fas fa-save" aria-hidden="true"></i>').attr('id', hour).on('click', function (e) {
+        saveText(this.getAttribute('id'))
+      });
+
+      timeBlock.append(hourDiv);
+      timeBlock.append(textarea);
+      timeBlock.append(saveButton);
+      $("#time-block-container").append(timeBlock);
     }
-  
-    // Function to display message in the header
-    function displayMessage(message) {
-      $("#currentDay").after("<p class='text-success'>" + message + "</p>");
+  }
+
+  // Function to save the text input into local storage
+  function saveText(hour) {
+    var textarea = $("#hour-" + hour).find("textarea")[0];
+    var text = textarea.value.trim();
+
+    // If statement that checks if the text is not empty
+    if (text !== "") {
+      localStorage.setItem("hour-" + hour, text);
+      displayMessage("Appointment added to local storage ✅", true);
     }
-  
-      // Initial update of the date in the header
+  }
+
+  // Function to load the saved text from local storage and display it on the page
+  function loadSavedText() {
+    for (var hour = 9; hour <= 17; hour++) {
+      var text = localStorage.getItem("hour-" + hour);
+      var textarea = $("#hour-" + hour).find("textarea")[0];
+
+      if (text) {
+        textarea.value = text;
+      }
+    }
+  }
+
+  // Generate time blocks and load saved text when the page loads
+  generateTimeBlocks();
+  loadSavedText();
+
+  // Update the hourly blocks every minute to reflect the current time
+  setInterval(function () {
     updateDate();
-  
-    // Add a listener for click events on the save button
-    $(".saveBtn").on("click", function () {
-          // Get the ID of the parent time block
-      var timeBlockId = $(this).parent().attr("id");
-      // Get the text from the textarea sibling of the clicked button
-      var eventText = $(this).siblings(".description").val();
-  
-      // Save the event text to localStorage using the time block ID as the key
-      localStorage.setItem(timeBlockId, eventText);
-  
-      // Display message in the header
-      displayMessage("Appointment added to local storage");
-    });
-  
-    // Function to update the styling of hourly time blocks based on the current time
-    function updateHourlyBlocks() {
-      // order of hours
-      var hoursOrder = [9, 10, 11, 12, 1, 2, 3, 4, 5];
-    
-      // Get current hour using Day.js
-      var currentHour = dayjs().hour();
-    
-      // Iterate through each time block
-      $(".time-block").each(function () {
-        // Get the hour from the time block ID
-        var blockHour = parseInt($(this).attr("id").split("-")[1]);
-        // Get the index of the block hour in the ordered array
-        var blockIndex = hoursOrder.indexOf(blockHour);
-        // Get the index of the current hour in the ordered array
-        var currentIndex = hoursOrder.indexOf(currentHour);
-  
-        // Remove existing styling classes
-        $(this).removeClass("past present future");
-    
-        // Add appropriate styling class based on the relationship between block and current hours
-        if (blockIndex < currentIndex) {
-          $(this).addClass("past");
-        } else if (blockIndex === currentIndex) {
-          $(this).addClass("present");
-        } else {
-          $(this).addClass("future");
-        }
-      });
-    }
-  
-    // Function to load saved events from localStorage and set textarea values
-    function loadSavedEvents() {
-      $(".time-block").each(function () {
-        // Get the saved event from localStorage based on the time block ID
-        var savedEvent = localStorage.getItem($(this).attr("id"));
-        // Set the textarea value with the saved event text
-        $(this).find(".description").val(savedEvent);
-      });
-    }
-    
-    // Initial update of hourly blocks and loading of saved events
-    updateHourlyBlocks();
-    loadSavedEvents();
-  
-    // Update the hourly blocks every minute to reflect the current time
-    setInterval(function () {
-      updateHourlyBlocks();
-      updateDate(); // Update the date every minute
-    }, 60000);
-  });
+  }, 60000);
+});
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Cool Daily Planner
+
+## Description
+The Cool Daily Planner is a simple and intuitive calendar app designed to help users manage their daily schedules efficiently. The motivation behind creating this planner was to provide a user-friendly tool that allows individuals with busy schedules to easily add and organize important events throughout the day.
+
+## Usage
+
+To use the Cool Daily Planner:
+
+1. Open the planner in your web browser.
+
+2. The current day will be prominently displayed at the top of the page.
+
+3. Scroll down to view time blocks for each hour from 9 am to 5 pm.
+
+4. Each time block is color-coded to represent whether it is in the past, present, or future.
+
+5. Click into a time block to enter an event for that specific hour.
+
+6. After entering the event, click the "Save" button for that time block to save the text in local storage.
+
+7. Saved events persist even after refreshing the page.
+
+## License
+
+MIT Liscense
+
+## Features
+
+1. Dynamic Date Display: Shows the current day dynamically using Day.js.
+
+2. Color-Coded Time Blocks: Visual indication of past, present, or future time blocks.
+
+3. Event Saving and Local Storage: Enter and save events with persistent storage in the local browser.
+
+4. Automatic Time Block Styling: Real-time updates of time block styles based on the current time.
+
+5. User-Friendly Interface: Easy event entry and saving with a straightforward "Save" button.
+
+6. Periodic Updates: Automatic refreshing of time block styles and the displayed date every minute.
+
+7. Device Compatibility: Responsive layout for use on desktops, tablets, and mobile phones.
+
